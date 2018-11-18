30bd708202e597499286ea1bb1088faeb5612033
oneish <stuartdavis@oneish.com>
2018-11-18 12:19:23 +0000

    added class Weak

--- strange.h
+++ strange.h
@@ -35,6 +35,7 @@ namespace strange
 	template <typename C> class IteratorCopy;
 	template <typename C> class IteratorRef;
 	class Reference;
+	class Weak;
 	template <typename D> class Data;
 	class Lake;
 	class Number;
@@ -2453,6 +2454,11 @@ public:
 		return mut_(_ptr);
 	}
 
+	virtual inline const Ptr clone_() const override
+	{
+		return mut_(_ptr->copy_());
+	}
+
 	virtual inline const Ptr iterator_() const override
 	{
 		return Iterator::mut_(me_());
@@ -2628,6 +2634,220 @@ private:
 
 	private:
 		Ptr _reference;
+	};
+};
+
+//----------------------------------------------------------------------
+class Weak : public Mutable
+//----------------------------------------------------------------------
+{
+public:
+	inline Weak(const Ptr ptr)
+		: Mutable{}
+		, _ptr{ ptr }
+	{
+	}
+
+	static inline const Ptr mut(const Ptr it)
+	{
+		return mut_(it->next_());
+	}
+
+	static inline const Ptr mut_(const Ptr ptr)
+	{
+		return make_<Weak>(ptr);
+	}
+
+	virtual inline const Ptr copy_() const override
+	{
+		return mut_(get_());
+	}
+
+	virtual inline const Ptr clone_() const override
+	{
+		return mut_(get_()->copy_());
+	}
+
+	virtual inline const Ptr iterator_() const override
+	{
+		return Iterator::mut_(me_());
+	}
+
+	virtual inline const Ptr pub_() const override
+	{
+		static const Ptr PUB = [this]()
+		{
+			const Ptr pub = Thing::pub_()->copy_();
+			Shoal* const shoal = static_<Shoal>(pub);
+			shoal->update_("stat", Static::fin_(&Weak::stat));
+			shoal->update_("mut", Static::fin_(&Weak::mut, "thing"));
+			shoal->update_("set", Member<Weak>::fin_(&Weak::set));
+			shoal->update_("get", Const<Weak>::fin_(&Weak::get));
+			shoal->finalize_();
+			return pub;
+		}();
+		return PUB;
+	}
+
+	static inline const Ptr stat_()
+	{
+		static const Ptr STAT = []()
+		{
+			const Ptr stat = Shoal::mut_();
+			Shoal* const shoal = static_<Shoal>(stat);
+			shoal->update_("stat", Static::fin_(&Weak::stat));
+			shoal->update_("mut", Static::fin_(&Weak::mut, "thing"));
+			shoal->finalize_();
+			return stat;
+		}();
+		return STAT;
+	}
+
+	static inline const Ptr stat(const Ptr ignore)
+	{
+		return stat_();
+	}
+
+	inline void set_(const Ptr ptr)
+	{
+		_ptr = ptr;
+	}
+
+	inline const Ptr set(const Ptr it)
+	{
+		const Ptr ptr = it->next_();
+		set_(ptr);
+		return ptr;
+	}
+
+	inline const Ptr get_() const
+	{
+		const Ptr ptr = _ptr.lock();
+		if (ptr)
+		{
+			return ptr;
+		}
+		return nothing_();
+	}
+
+	inline const Ptr get(const Ptr ignore) const
+	{
+		return get_();
+	}
+
+	virtual inline const Ptr type_() const override
+	{
+		static const Ptr TYPE = sym_("strange::Weak");
+		return TYPE;
+	}
+
+	virtual inline const Ptr cats_() const override
+	{
+		static const Ptr CATS = []()
+		{
+			const Ptr cats = Herd::mut_();
+			Herd* const herd = static_<Herd>(cats);
+			herd->insert_("strange::Mutable");
+			herd->insert_("strange::Thing");
+			herd->finalize_();
+			return cats;
+		}();
+		return CATS;
+	}
+
+private:
+	WeakPtr _ptr;
+
+	class Iterator : public Mutable
+	{
+	public:
+		inline Iterator(const Ptr weak)
+			: Mutable{}
+			, _weak{ weak }
+		{
+		}
+
+		static inline const Ptr mut(const Ptr it)
+		{
+			return mut_(it->next_());
+		}
+
+		static inline const Ptr mut_(const Ptr weak)
+		{
+			make_<Iterator>(weak);
+		}
+
+		virtual inline const Ptr copy_() const override
+		{
+			return mut_(_weak);
+		}
+
+		virtual inline const Ptr next_() override
+		{
+			if (_weak->is_("."))
+			{
+				return _weak;
+			}
+			const Ptr ptr = static_<Weak>(_weak)->get_();
+			_weak = stop_();
+			return ptr;
+		}
+
+		virtual inline const Ptr pub_() const override
+		{
+			static const Ptr PUB = [this]()
+			{
+				const Ptr pub = Thing::pub_()->copy_();
+				Shoal* const shoal = static_<Shoal>(pub);
+				shoal->update_("stat", Static::fin_(&Iterator::stat));
+				shoal->update_("mut", Static::fin_(&Iterator::mut, "thing"));
+				shoal->finalize_();
+				return pub;
+			}();
+			return PUB;
+		}
+
+		static inline const Ptr stat_()
+		{
+			static const Ptr STAT = []()
+			{
+				const Ptr stat = Shoal::mut_();
+				Shoal* const shoal = static_<Shoal>(stat);
+				shoal->update_("stat", Static::fin_(&Iterator::stat));
+				shoal->update_("mut", Static::fin_(&Iterator::mut, "thing"));
+				shoal->finalize_();
+				return stat;
+			}();
+			return STAT;
+		}
+
+		static inline const Ptr stat(const Ptr ignore)
+		{
+			return stat_();
+		}
+
+		virtual inline const Ptr type_() const override
+		{
+			static const Ptr TYPE = sym_("strange::Weak::Iterator");
+			return TYPE;
+		}
+
+		virtual inline const Ptr cats_() const override
+		{
+			static const Ptr CATS = []()
+			{
+				const Ptr cats = Herd::mut_();
+				Herd* const herd = static_<Herd>(cats);
+				herd->insert_("strange::Mutable");
+				herd->insert_("strange::Thing");
+				herd->finalize_();
+				return cats;
+			}();
+			return CATS;
+		}
+
+	private:
+		Ptr _weak;
 	};
 };
 
@@ -8560,6 +8780,10 @@ inline void Herd::replace_links_(const Thing::Ptr shoal)
 
 //======================================================================
 // class Reference
+//======================================================================
+
+//======================================================================
+// class Weak
 //======================================================================
 
 //======================================================================
