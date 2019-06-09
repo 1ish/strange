17dcac8e26967ed0991f3c7bac5d8560dae9872b
oneish <stuartdavis@oneish.com>
2019-06-09 10:21:32 +0100

    refactored expression_cat

--- sauce/strange_expression_cat_t.h
+++ sauce/strange_expression_cat_t.h
@@ -14,7 +14,7 @@ public: ___STRANGE_EXPRESSION___
 		forward_const_iterator_a<> it = terms.cbegin_();
 		if (it == terms.cend_())
 		{
-			return val(token, sym(""), no(), no(), no(), no());
+			return val(token, 0);
 		}
 		any_a<> name = *it;
 		if (!check<symbol_a<>>(name))
@@ -23,7 +23,7 @@ public: ___STRANGE_EXPRESSION___
 		}
 		if (++it == terms.cend_())
 		{
-			return val(token, cast<symbol_a<>>(name), no(), no(), no(), no());
+			return val(token, 1, cast<symbol_a<>>(name));
 		}
 		any_a<> arguments = *it;
 		if (!check<expression_a<>>(arguments))
@@ -32,9 +32,7 @@ public: ___STRANGE_EXPRESSION___
 		}
 		if (++it == terms.cend_())
 		{
-			return val(token, cast<symbol_a<>>(name),
-				cast<expression_a<>>(arguments),
-				no(), no(), no());
+			return val(token, 2, cast<symbol_a<>>(name), cast<expression_a<>>(arguments));
 		}
 		any_a<> parameters = *it;
 		if (!check<expression_a<>>(parameters))
@@ -43,10 +41,7 @@ public: ___STRANGE_EXPRESSION___
 		}
 		if (++it == terms.cend_())
 		{
-			return val(token, cast<symbol_a<>>(name),
-				cast<expression_a<>>(arguments),
-				cast<expression_a<>>(parameters),
-				no(), no());
+			return val(token, 3, cast<symbol_a<>>(name), cast<expression_a<>>(arguments), cast<expression_a<>>(parameters));
 		}
 		any_a<> result = *it;
 		if (!check<expression_a<>>(result))
@@ -55,22 +50,36 @@ public: ___STRANGE_EXPRESSION___
 		}
 		if (++it == terms.cend_())
 		{
-			return val(token, cast<symbol_a<>>(name),
+			return val(token, 4, cast<symbol_a<>>(name),
 				cast<expression_a<>>(arguments),
 				cast<expression_a<>>(parameters),
-				cast<expression_a<>>(result),
-				no());
+				cast<expression_a<>>(result));
 		}
-		return val(token, cast<symbol_a<>>(name),
+		return val(token, 5, cast<symbol_a<>>(name),
 			cast<expression_a<>>(arguments),
 			cast<expression_a<>>(parameters),
 			cast<expression_a<>>(result),
 			*it);
 	}
 
-	static inline expression_a<> val(token_a<> const& token, symbol_a<> const& name, any_a<> const& arguments, any_a<> const& parameters, any_a<> const& result, any_a<> const& reference)
+	static inline expression_a<> val(token_a<> const& token, int64_t count, symbol_a<> const& name = sym(""))
 	{
-		return expression_a<>{ expression_cat_t<>(token, name, arguments, parameters, result, reference) };
+		return expression_a<>{ expression_cat_t<>(token, count, name, expression_t<>::val(token), expression_t<>::val(token), expression_t<>::val(token), no()) };
+	}
+
+	static inline expression_a<> val(token_a<> const& token, int64_t count, symbol_a<> const& name, expression_a<> const& arguments)
+	{
+		return expression_a<>{ expression_cat_t<>(token, count, name, arguments, expression_t<>::val(token), expression_t<>::val(token), no()) };
+	}
+
+	static inline expression_a<> val(token_a<> const& token, int64_t count, symbol_a<> const& name, expression_a<> const& arguments, expression_a<> const& parameters)
+	{
+		return expression_a<>{ expression_cat_t<>(token, count, name, arguments, parameters, expression_t<>::val(token), no()) };
+	}
+
+	static inline expression_a<> val(token_a<> const& token, int64_t count, symbol_a<> const& name, expression_a<> const& arguments, expression_a<> const& parameters, expression_a<> const& result, any_a<> const& reference = no())
+	{
+		return expression_a<>{ expression_cat_t<>(token, count, name, arguments, parameters, result, reference) };
 	}
 
 	// reflection
@@ -87,29 +96,29 @@ public: ___STRANGE_EXPRESSION___
 	// function
 	inline any_a<> operate_(any_a<>& thing, range_a<> const& range) const
 	{
-		if (!check<expression_a<>>(_arguments))
+		if (_count <= 1)
 		{
 			return cat_t<>::val_(_name);
 		}
-		auto arguments = cast<expression_a<>>(_arguments).operate_(thing, range);
+		auto arguments = _arguments.operate_(thing, range);
 		if (!check<flock_a<>>(arguments))
 		{
 			throw dis(_token.report() + "strange::expression_cat::operate arguments are not a flock");
 		}
-		if (!check<expression_a<>>(_parameters))
+		if (_count == 2)
 		{
 			return cat_t<>::val_(_name, cast<flock_a<>>(arguments));
 		}
-		auto parameters = cast<expression_a<>>(_parameters).operate_(thing, range);
+		auto parameters = _parameters.operate_(thing, range);
 		if (!check<flock_a<>>(parameters))
 		{
 			throw dis(_token.report() + "strange::expression_cat::operate parameters are not a flock");
 		}
-		if (!check<expression_a<>>(_result))
+		if (_count == 3)
 		{
 			return cat_t<>::val_(_name, cast<flock_a<>>(arguments), cast<flock_a<>>(parameters));
 		}
-		auto result = cast<expression_a<>>(_result).operate_(thing, range);
+		auto result = _result.operate_(thing, range);
 		if (!check<symbol_a<>>(result))
 		{
 			throw dis(_token.report() + "strange::expression_cat::operate result is not a symbol");
@@ -121,13 +130,13 @@ public: ___STRANGE_EXPRESSION___
 	inline void generate(int64_t indent, river_a<>& river) const
 	{
 		river.write_(lake_from_string(" <" + _name.to_string()));
-		if (check<expression_a<>>(_arguments))
+		if (_count >= 2)
 		{
-			cast<expression_a<>>(_arguments).generate(indent, river);
-			if (check<expression_a<>>(_parameters))
+			_arguments.generate(indent, river);
+			if (_count >= 3)
 			{
 				auto params = river_t<>::val();
-				cast<expression_a<>>(_parameters).generate(indent, params);
+				_parameters.generate(indent, params);
 				auto str = params.to_string();
 				auto len = str.length();
 				if (len >= 4 && str[1] == '[' && str[len - 2] == ']')
@@ -136,9 +145,9 @@ public: ___STRANGE_EXPRESSION___
 					str[len - 2] = ')';
 					river.write_(lake_from_string(str));
 				}
-				if (check<expression_a<>>(_result))
+				if (_count >= 4)
 				{
-					cast<expression_a<>>(_result).generate(indent, river);
+					_result.generate(indent, river);
 					if (_reference)
 					{
 						river.write_(lake_from_string(">& "));
@@ -153,18 +162,18 @@ public: ___STRANGE_EXPRESSION___
 	inline void generate_cpp(int64_t indent, river_a<>& river) const
 	{
 		river.write_(lake_from_string(" strange::cat_t<>::val(\"" + _name.to_string() + "\""));
-		if (check<expression_a<>>(_arguments))
+		if (_count >= 2)
 		{
 			river.write_(lake_from_string(","));
-			cast<expression_a<>>(_arguments).generate_cpp(indent, river);
-			if (check<expression_a<>>(_parameters))
+			_arguments.generate_cpp(indent, river);
+			if (_count >= 3)
 			{
 				river.write_(lake_from_string(","));
-				cast<expression_a<>>(_parameters).generate_cpp(indent, river);
-				if (check<expression_a<>>(_result))
+				_parameters.generate_cpp(indent, river);
+				if (_count >= 4)
 				{
 					river.write_(lake_from_string(","));
-					cast<expression_a<>>(_result).generate_cpp(indent, river);
+					_result.generate_cpp(indent, river);
 					if (_reference)
 					{
 						river.write_(lake_from_string(", true"));
@@ -176,14 +185,16 @@ public: ___STRANGE_EXPRESSION___
 	}
 
 protected:
+	int64_t const _count;
 	symbol_a<> const _name;
-	any_a<> const _arguments;
-	any_a<> const _parameters;
-	any_a<> const _result;
+	expression_a<> const _arguments;
+	expression_a<> const _parameters;
+	expression_a<> const _result;
 	any_a<> const _reference;
 
-	inline expression_cat_t(token_a<> const& token, symbol_a<> const& name, any_a<> const& arguments, any_a<> const& parameters, any_a<> const& result, any_a<> const& reference)
+	inline expression_cat_t(token_a<> const& token, int64_t count, symbol_a<> const& name, expression_a<> const& arguments, expression_a<> const& parameters, expression_a<> const& result, any_a<> const& reference)
 		: expression_t{ token }
+		, _count{ count }
 		, _name{ name }
 		, _arguments{ arguments }
 		, _parameters{ parameters }
