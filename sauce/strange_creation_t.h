e219a63e86f17deff2ba4c487c0d181b5c13c68c
oneish <stuartdavis@oneish.com>
2019-08-15 17:31:38 +0100

    move creation val processing into constructor

--- sauce/strange_creation_t.h
+++ sauce/strange_creation_t.h
@@ -12,20 +12,9 @@ public:
 	using over = collection_o<creation_t<>>;
 
 	// construction
-	static inline any_a<> val__(range_a<> const& parents)
+	static inline any_a<> val__(range_a<> const& range)
 	{
-		//TODO move into constructor
-		auto child = over{ creation_t<>{} };
-		for (auto const& parent : parents)
-		{
-			if (!check<unordered_shoal_a<>>(parent))
-			{
-				throw dis("strange::creation::val passed non-unordered-shoal parent");
-			}
-			child.merge(cast<unordered_shoal_a<>>(parent));
-		}
-		//TODO finishing touches?
-		return unordered_shoal_a<>{ child };
+		return unordered_shoal_a<>{ over{ creation_t<>{ range } } };
 	}
 
 	template <typename... Args>
@@ -107,10 +96,20 @@ public:
 protected:
 	symbol_a<> _type;
 
-	inline creation_t()
+	inline creation_t(range_a<> const& parents)
 		: unordered_shoal_t{ std_unordered_map_any_any{} }
 		, _type(sym(""))
-	{}
+	{
+		for (auto const& parent : parents)
+		{
+			if (!check<unordered_shoal_a<>>(parent))
+			{
+				throw dis("strange::creation::val passed non-unordered-shoal parent");
+			}
+			merge(cast<unordered_shoal_a<>>(parent));
+		}
+		//TODO finishing touches?
+	}
 
 private:
 	static bool const ___share___;
