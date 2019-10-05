578e1a36dafa7b170766481053b48a2a3a90b28a
oneish <stuartdavis@oneish.com>
2019-10-05 12:07:54 +0100

    more tastes

---
+++ taste/strange_any_taste.h
@@ -0,0 +1,5 @@
+TEST_CASE("strange any taste", "[default constructor]")
+{
+	any_a<> a;
+	REQUIRE(1 == 1);
+}
--- taste/strange_taste.cpp
+++ taste/strange_taste.cpp
@@ -6,13 +6,6 @@
 
 #include "../sauce/strange.h"
 
-unsigned int Factorial(unsigned int number) {
-	return number <= 1 ? number : Factorial(number - 1) * number;
-}
+using namespace strange;
 
-TEST_CASE("Factorials are computed", "[factorial]") {
-	REQUIRE(Factorial(1) == 1);
-	REQUIRE(Factorial(2) == 2);
-	REQUIRE(Factorial(3) == 6);
-	REQUIRE(Factorial(10) == 3628800);
-}
+#include "strange_any_taste.h"
