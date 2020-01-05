1adae574e8e2530367a131ab337f0ff2d04df24e
oneish <stuartdavis@oneish.com>
2020-01-05 15:09:40 +0000

    all tests passing, but without any mutation of interpreted local variables

--- sauce/strange_thing_t.h
+++ sauce/strange_thing_t.h
@@ -99,7 +99,7 @@ public:
 		{
 			throw dis("strange::thing::invoke passed empty range");
 		}
-		any_a<> thing(*it, true);
+		any_a<> thing = *it; //TODO reference, true);
 		if (++it == range.cend_())
 		{
 			throw dis("strange::thing::invoke passed short range");
@@ -135,7 +135,7 @@ public:
 		{
 			throw dis("strange::thing::operate passed empty range");
 		}
-		any_a<> thing(*it, true);
+		any_a<> thing = *it; //TODO reference, true);
 		return thing.operate(thing, range_t<>::create_(++it, range.cend_()));
 	}
 
