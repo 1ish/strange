d71733f981f43503492384aa8f2ae4fa735e4fcd
oneish <stuartdavis@oneish.com>
2020-02-05 07:59:54 +0000

    codegen

--- sauce/strange_expression_abstraction_t.h
+++ sauce/strange_expression_abstraction_t.h
@@ -599,11 +599,20 @@ protected:
 
 	inline void _define_common_class_nonvirtual_native_member_(std::string const& name, std::string const& value, river_a<>& river) const
 	{
-		river.write_string("\t// native: " + name + " " + value + "\n");
 		std::string return_type;
 		std::string arguments;
 		std::string parameters;
 		std::string constness;
+		_parse_native_member_definition_(value, return_type, arguments, parameters, constness);
+		river.write_string("\t// native: " + name + " " + value + "\n");
+		river.write_string("\t// return_type: " + return_type + "\n");
+		river.write_string("\t// arguments: " + arguments + "\n");
+		river.write_string("\t// parameters: " + parameters + "\n");
+		river.write_string("\t// constness: " + constness + "\n");
+	}
+
+	inline void _parse_native_member_definition_(std::string const& value, std::string& return_type, std::string& arguments, std::string& parameters, std::string& constness) const
+	{
 		auto tokenizer = tokenizer_t<>::create_(river_t<>::create(value));
 		int64_t toke = 1;
 		std::string parameter;
@@ -655,10 +664,6 @@ protected:
 				break;
 			}
 		}
-		river.write_string("\t// return_type: " + return_type + "\n");
-		river.write_string("\t// arguments: " + arguments + "\n");
-		river.write_string("\t// parameters: " + parameters + "\n");
-		river.write_string("\t// constness: " + constness + "\n");
 	}
 
 private:
