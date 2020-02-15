9f3c62f7588031928085d79842e1c1fad94bd5c5
oneish <stuartdavis@oneish.com>
2020-02-15 12:48:08 +0000

    added new forward_const_iterator_a

--- taste/strange_iterators_generate_cpp.h
+++ taste/strange_iterators_generate_cpp.h
@@ -101,3 +101,23 @@ TEST_CASE("strange parse and generate cpp file bidirectional_iterator_data abstr
 		throw std::runtime_error(m.to_string());
 	}
 }
+
+TEST_CASE("strange parse and generate cpp file forward_const_iterator abstraction", "[parse_t]")
+{
+	try
+	{
+		auto parser = parser_t<>::create_();
+		auto expression = parser.parse_(tokenizer_t<>::create_(river_t<>::file(abstractions_dir +
+			"strange_forward_const_iterator_a.str")));
+		auto river = river_t<>::file(generate_cpp_dir +
+			"declare_strange_forward_const_iterator_a.hpp", false, true, true); // in, out, trunc
+		expression.generate_cpp(1, 1, river, true, false); // declare
+		river = river_t<>::file(generate_cpp_dir +
+			"define_strange_forward_const_iterator_a.hpp", false, true, true); // in, out, trunc
+		expression.generate_cpp(1, 1, river, false, true); // define
+	}
+	catch (misunderstanding_a<>& m)
+	{
+		throw std::runtime_error(m.to_string());
+	}
+}
