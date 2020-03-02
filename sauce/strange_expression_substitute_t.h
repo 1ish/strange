4f67792a4aa6ffe784a95c9e329d3d5300dfd12d
oneish <stuartdavis@oneish.com>
2020-03-02 18:15:47 +0000

    use fast_ref

--- sauce/strange_expression_substitute_t.h
+++ sauce/strange_expression_substitute_t.h
@@ -49,7 +49,7 @@ public:
 		auto const thing = expression.evaluate_();
 		if (check<operation_a<>>(_thing) && check<operation_a<>>(thing))
 		{
-			cast_ref<operation_a<>>(_thing).assign(cast<operation_a<>>(thing));
+			fast_ref<operation_a<>>(_thing).assign(fast<operation_a<>>(thing));
 		}
 		_thing = thing;
 	}
