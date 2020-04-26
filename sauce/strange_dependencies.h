03b710f97cd8121bf2b057aa45a6a48b8b76e116
oneish <stuartdavis@oneish.com>
2020-04-26 05:25:38 +0100

    changed dart implementation to use unsafe pointers

--- sauce/strange_dependencies.h
+++ sauce/strange_dependencies.h
@@ -103,7 +103,7 @@ namespace strange
 
 	using std_exception = std::exception;
 
-	using dart_packet = dart::packet;
+	using dart_packet = dart::basic_packet<dart::unsafe_ptr>;
 
 	inline dart_packet dart_packet_from_vector(std_vector<int8_t> const& binary) { return dart_packet{ gsl::make_span(reinterpret_cast<gsl::byte const*>(binary.data()), binary.size()) }; }
 	inline dart_packet dart_packet_from_string(std_string const& binary) { return dart_packet{ gsl::make_span(reinterpret_cast<gsl::byte const*>(binary.data()), binary.size()) }; }
