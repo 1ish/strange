3967484541c93f4a9e38c89ea3e7ebebb71e0806
oneish <stuartdavis@oneish.com>
2021-05-11 17:23:06 +0100

    comments only

--- sauce/core/fundamentals/things/strange__symbol_t.h
+++ sauce/core/fundamentals/things/strange__symbol_t.h
@@ -13,51 +13,76 @@ extern "C"
 		STRANGE__SYMBOL_D
 	};
 
+	// symbol_o
 	strange__symbol_o const* strange__symbol_o_f();
 	strange__symbol_o const* strange__symbol_p_f();
 	strange__symbol_o const* strange__symbol_n_f();
+
+	// any_a
 	void strange__symbol___free_f(void const* const me /* <symbol># */);
+
 	void strange__symbol___copy_f(void const* const me /* <symbol># */,
 		void* const cp /* <symbol>= */);
+
 	void strange__symbol___clone_f(void const* const me /* <symbol># */,
 		void* const cp /* <symbol>= */);
+
 	bool strange__symbol__is_f(void const* const me /* <symbol># */,
 		void const* const ab /* <any># */);
+
 	bool strange__symbol__as_f(void const* const me /* <symbol># */,
 		void* const ab /* <any>= */);
+
 	void strange__symbol___as_f(void const* const me /* <symbol># */,
 		void* const ab /* <any>= */);
+
 	strange__symbol_a strange__symbol__type_f(void const* const me /* <symbol># */);
+
 	bool strange__symbol__equal_f(void const* const me /* <symbol># */,
 		void const* const other /* <any># */);
+
 	bool strange__symbol___equal_f(void const* const me /* <symbol># */,
 		void const* const other /* <symbol># */);
+
 	bool strange__symbol__not_equal_f(void const* const me /* <symbol># */,
 		void const* const other /* <any># */);
+
 	bool strange__symbol___not_equal_f(void const* const me /* <symbol># */,
 		void const* const other /* <symbol># */);
+
 	bool strange__symbol__less_f(void const* const me /* <symbol># */,
 		void const* const other /* <any># */);
+
 	bool strange__symbol___less_f(void const* const me /* <symbol># */,
 		void const* const other /* <symbol># */);
+
 	bool strange__symbol__greater_f(void const* const me /* <symbol># */,
 		void const* const other /* <any># */);
+
 	bool strange__symbol___greater_f(void const* const me /* <symbol># */,
 		void const* const other /* <symbol># */);
+
 	bool strange__symbol__less_or_equal_f(void const* const me /* <symbol># */,
 		void const* const other /* <any># */);
+
 	bool strange__symbol___less_or_equal_f(void const* const me /* <symbol># */,
 		void const* const other /* <symbol># */);
+
 	bool strange__symbol__greater_or_equal_f(void const* const me /* <symbol># */,
 		void const* const other /* <any># */);
+
 	bool strange__symbol___greater_or_equal_f(void const* const me /* <symbol># */,
 		void const* const other /* <symbol># */);
 
+	// symbol_a
 	strange__symbol_a strange__symbol__add_f(void const* const me /* <symbol># */,
 		void const* const symbol /* <symbol># */);
+
 	char const* strange__symbol___to_char_star_f(void const* const me /* <symbol># */);
+
 	size_t strange__symbol__length_f(void const* const me /* <symbol># */);
 
+	// constructors
 	strange__symbol_a sym(char const* const s);
 	strange__symbol_a no_sym();
 }
--- sauce/core/fundamentals/things/strange__thing_t.h
+++ sauce/core/fundamentals/things/strange__thing_t.h
@@ -11,49 +11,74 @@ extern "C"
 		STRANGE__THING_D
 	};
 
+	// any_o
 	strange__any_o const* strange__thing_o_f();
 	strange__any_o const* strange__thing_p_f();
 	strange__any_o const* strange__thing_n_f();
+
+	// any_a
 	void strange__thing___free_f(void const* const me /* <any># */);
+
 	void strange__thing___copy_f(void const* const me /* <any># */,
 		void* const cp /* <any>= */);
+
 	void strange__thing___no_copy_f(void const* const me /* <any># */,
 		void* const cp /* <any>= */);
+
 	bool strange__thing__is_f(void const* const me /* <any># */,
 		void const* const ab /* <any># */);
+
 	bool strange__thing__as_f(void const* const me /* <any># */,
 		void* const ab /* <any>= */);
+
 	void strange__thing___as_f(void const* const me /* <any># */,
 		void* const ab /* <any>= */);
+
 	strange__symbol_a strange__thing__type_f(void const* const me /* <any># */);
+
 	bool strange__thing__something_f(void const* const me /* <any># */);
+
 	bool strange__thing__nothing_f(void const* const me /* <any># */);
+
 	bool strange__thing__equal_f(void const* const me /* <any># */,
 		void const* const other /* <any># */);
+
 	bool strange__thing___equal_f(void const* const me /* <any># */,
 		void const* const other /* <any># */);
+
 	bool strange__thing__not_equal_f(void const* const me /* <any># */,
 		void const* const other /* <any># */);
+
 	bool strange__thing___not_equal_f(void const* const me /* <any># */,
 		void const* const other /* <any># */);
+
 	bool strange__thing__less_f(void const* const me /* <any># */,
 		void const* const other /* <any># */);
+
 	bool strange__thing___less_f(void const* const me /* <any># */,
 		void const* const other /* <any># */);
+
 	bool strange__thing__greater_f(void const* const me /* <any># */,
 		void const* const other /* <any># */);
+
 	bool strange__thing___greater_f(void const* const me /* <any># */,
 		void const* const other /* <any># */);
+
 	bool strange__thing__less_or_equal_f(void const* const me /* <any># */,
 		void const* const other /* <any># */);
+
 	bool strange__thing___less_or_equal_f(void const* const me /* <any># */,
 		void const* const other /* <any># */);
+
 	bool strange__thing__greater_or_equal_f(void const* const me /* <any># */,
 		void const* const other /* <any># */);
+
 	bool strange__thing___greater_or_equal_f(void const* const me /* <any># */,
 		void const* const other /* <any># */);
 
 	void mutate(void* const me /* <any># */);
+
+	// constructors
 	strange__any_a something();
 	strange__any_a nothing();
 }
