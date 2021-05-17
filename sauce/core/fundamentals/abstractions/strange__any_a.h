9136c57f8aace5a6f2c36ebb914172985423a7b9
oneish <stuartdavis@oneish.com>
2021-05-17 19:39:29 +0100

    remove some #defines

--- sauce/core/fundamentals/abstractions/strange__any_a.h
+++ sauce/core/fundamentals/abstractions/strange__any_a.h
@@ -1,83 +1,80 @@
 #ifndef COM_ONEISH__STRANGE__ANY_A_H
 #define COM_ONEISH__STRANGE__ANY_A_H
 
-#define STRANGE__ANY_O \
-		strange__symbol_a (*cat)(void const* const me /* :<any># */); \
-\
-		void (*_free)(void const* const me /* :<any># */); \
-\
-		void (*_copy)(void const* const me /* :<any># */, \
-			void* const cp /* :<any>= */); \
-\
-		bool (*is)(void const* const me /* :<any># */, \
-			void const* const ab /* :<any># */); \
-\
-		bool (*as)(void const* const me /* :<any># */, \
-			void* const ab /* :<any>= */); \
-\
-		void (*_as)(void const* const me /* :<any># */, \
-			void* const ab /* :<any>= */); \
-\
-		strange__symbol_a (*type)(void const* const me /* :<any># */); \
-\
-		void (*_set_pointer)(void* const me /* :<any>= */, \
-			bool is_pointer /* :_bool_# */ ); \
-\
-		bool (*_pointer)(void const* const me /* :<any># */); \
-\
-		void (*set_something)(void* const me /* :<any>= */, \
-			bool is_something /* :_bool_# */); \
-\
-		bool (*something)(void const* const me /* :<any># */); \
-\
-		void (*set_error)(void* const me /* :<any>= */, \
-			void const* const error /* :<any># */); \
-\
-		strange__any_a (*error)(void const* const me /* :<any># */); \
-\
-		uint64_t (*hash)(void const* const me /* :<any># */); \
-\
-		bool (*equal)(void const* const me /* :<any># */, \
-			void const* const other /* :<any># */); \
-\
-		bool (*_equal)(void const* const me /* :<any># */, \
-			void const* const other /* :<any># */); \
-\
-		bool (*not_equal)(void const* const me /* :<any># */, \
-			void const* const other /* :<any># */); \
-\
-		bool (*_not_equal)(void const* const me /* :<any># */, \
-			void const* const other /* :<any># */); \
-\
-		bool (*less)(void const* const me /* :<any># */, \
-			void const* const other /* :<any># */); \
-\
-		bool (*_less)(void const* const me /* :<any># */, \
-			void const* const other /* :<any># */); \
-\
-		bool (*greater)(void const* const me /* :<any># */, \
-			void const* const other /* :<any># */); \
-\
-		bool (*_greater)(void const* const me /* :<any># */, \
-			void const* const other /* :<any># */); \
-\
-		bool (*less_or_equal)(void const* const me /* :<any># */, \
-			void const* const other /* :<any># */); \
-\
-		bool (*_less_or_equal)(void const* const me /* :<any># */, \
-			void const* const other /* :<any># */); \
-\
-		bool (*greater_or_equal)(void const* const me /* :<any># */, \
-			void const* const other /* :<any># */); \
-\
-		bool (*_greater_or_equal)(void const* const me /* :<any># */, \
-			void const* const other /* :<any># */); \
-
 extern "C"
 {
 	struct strange__any_o
 	{
-		STRANGE__ANY_O
+		strange__symbol_a (*cat)(void const* const me /* :<any># */);
+
+		void (*_free)(void const* const me /* :<any># */);
+
+		void (*_copy)(void const* const me /* :<any># */,
+			void* const cp /* :<any>= */);
+
+		bool (*is)(void const* const me /* :<any># */,
+			void const* const ab /* :<any># */);
+
+		bool (*as)(void const* const me /* :<any># */,
+			void* const ab /* :<any>= */);
+
+		void (*_as)(void const* const me /* :<any># */,
+			void* const ab /* :<any>= */);
+
+		strange__symbol_a (*type)(void const* const me /* :<any># */);
+
+		void (*_set_pointer)(void* const me /* :<any>= */,
+			bool is_pointer /* :_bool_# */ );
+
+		bool (*_pointer)(void const* const me /* :<any># */);
+
+		void (*set_something)(void* const me /* :<any>= */,
+			bool is_something /* :_bool_# */);
+
+		bool (*something)(void const* const me /* :<any># */);
+
+		void (*set_error)(void* const me /* :<any>= */,
+			void const* const error /* :<any># */);
+
+		strange__any_a (*error)(void const* const me /* :<any># */);
+
+		uint64_t (*hash)(void const* const me /* :<any># */);
+
+		bool (*equal)(void const* const me /* :<any># */,
+			void const* const other /* :<any># */);
+
+		bool (*_equal)(void const* const me /* :<any># */,
+			void const* const other /* :<any># */);
+
+		bool (*not_equal)(void const* const me /* :<any># */,
+			void const* const other /* :<any># */);
+
+		bool (*_not_equal)(void const* const me /* :<any># */,
+			void const* const other /* :<any># */);
+
+		bool (*less)(void const* const me /* :<any># */,
+			void const* const other /* :<any># */);
+
+		bool (*_less)(void const* const me /* :<any># */,
+			void const* const other /* :<any># */);
+
+		bool (*greater)(void const* const me /* :<any># */,
+			void const* const other /* :<any># */);
+
+		bool (*_greater)(void const* const me /* :<any># */,
+			void const* const other /* :<any># */);
+
+		bool (*less_or_equal)(void const* const me /* :<any># */,
+			void const* const other /* :<any># */);
+
+		bool (*_less_or_equal)(void const* const me /* :<any># */,
+			void const* const other /* :<any># */);
+
+		bool (*greater_or_equal)(void const* const me /* :<any># */,
+			void const* const other /* :<any># */);
+
+		bool (*_greater_or_equal)(void const* const me /* :<any># */,
+			void const* const other /* :<any># */);
 	};
 
 	struct strange__any_a
--- sauce/core/fundamentals/abstractions/strange__symbol_a.h
+++ sauce/core/fundamentals/abstractions/strange__symbol_a.h
@@ -1,24 +1,20 @@
 #ifndef COM_ONEISH__STRANGE__SYMBOL_A_H
 #define COM_ONEISH__STRANGE__SYMBOL_A_H
 
-#define STRANGE__SYMBOL_O \
-		STRANGE__ANY_O \
-		strange__symbol_a (*add)(void const* const me /* :<symbol># */, \
-			void const* const suffix /* :<symbol># */); \
-\
-		char const* (*to_char_star)(void const* const me /* :<symbol># */); \
-\
-		int64_t (*length)(void const* const me /* :<symbol># */); \
-\
-		char (*first_char)(void const* const me /* :<symbol># */); \
-\
-		char (*last_char)(void const* const me /* :<symbol># */); \
-
 extern "C"
 {
-	struct strange__symbol_o
+	struct strange__symbol_o : strange__any_o
 	{
-		STRANGE__SYMBOL_O
+		strange__symbol_a (*add)(void const* const me /* :<symbol># */,
+			void const* const suffix /* :<symbol># */);
+
+		char const* (*to_char_star)(void const* const me /* :<symbol># */);
+
+		int64_t (*length)(void const* const me /* :<symbol># */);
+
+		char (*first_char)(void const* const me /* :<symbol># */);
+
+		char (*last_char)(void const* const me /* :<symbol># */);
 	};
 
 	struct strange__symbol_a
