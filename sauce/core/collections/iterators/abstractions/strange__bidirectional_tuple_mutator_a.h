a5db1810701678e12e069d87b43c856e84b39b56
oneish <stuartdavis@oneish.com>
2021-06-18 13:59:03 +0100

    added bidirectional_tuple_mutator_a

---
+++ sauce/core/collections/iterators/abstractions/strange__bidirectional_tuple_mutator_a.h
@@ -0,0 +1,36 @@
+#ifndef COM_ONEISH__STRANGE__BIDIRECTIONAL_TUPLE_MUTATOR_A_H
+#define COM_ONEISH__STRANGE__BIDIRECTIONAL_TUPLE_MUTATOR_A_H
+
+namespace strange
+{
+	template <typename... elements_d>
+	struct bidirectional_tuple_mutator_i
+	{
+		void (*decrement) (var<bidirectional_tuple_mutator_a<elements_d...>> const& me);
+	};
+
+	template <typename... elements_d>
+	struct bidirectional_tuple_mutator_o :
+		forward_tuple_mutator_o<elements_d...>,
+		bidirectional_tuple_mutator_i<elements_d...>
+	{
+	};
+
+	template <typename... elements_d>
+	struct bidirectional_tuple_mutator_a
+	{
+		using operations = bidirectional_tuple_mutator_o<elements_d...>;
+		using creator_fp = fit<bidirectional_tuple_mutator_a<elements_d...>>(*)(con<range_a<>> const& range);
+
+		mutable thing_t* t;
+		mutable operations const* o;
+
+		static var<symbol_a> cat(con<> const& me); //TODO cat
+
+		static creator_fp creator(con<symbol_a> const& scope,
+			con<symbol_a> const& thing,
+			con<symbol_a> const& function);
+	};
+}
+
+#endif
