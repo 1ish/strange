d0259dee0b69f94ff0819545163b9738fcfebb2f
oneish <stuartdavis@oneish.com>
2021-11-01 08:21:49 +0000

    added forward_mutator_t

---
+++ sauce/core/collections/iterators/things/strange__forward_mutator_t.cpp
@@ -0,0 +1,158 @@
+#include "../../../../strange.h"
+
+namespace strange
+{
+	// forward_mutator_t
+	// forward_mutator_o
+	template <typename iterator_d, typename element_d>
+	forward_mutator_o<element_d> const* forward_mutator_t<iterator_d, element_d>::_operations()
+	{
+		static forward_mutator_o<element_d> operations =
+		{
+			{
+				// any_a
+				forward_mutator_a<element_d>::cat,
+				forward_mutator_t<iterator_d, element_d>::is,
+				forward_mutator_t<iterator_d, element_d>::as,
+				forward_mutator_t<iterator_d, element_d>::type,
+				forward_mutator_t<iterator_d, element_d>::set_error,
+				forward_mutator_t<iterator_d, element_d>::error,
+				forward_mutator_t<iterator_d, element_d>::hash,
+				forward_mutator_t<iterator_d, element_d>::equal,
+				forward_mutator_t<iterator_d, element_d>::less,
+				forward_mutator_t<iterator_d, element_d>::less_or_equal,
+				forward_mutator_t<iterator_d, element_d>::pack,
+				forward_mutator_t<iterator_d, element_d>::_free,
+				forward_mutator_t<iterator_d, element_d>::_copy,
+				forward_mutator_t<iterator_d, element_d>::_set_pointer,
+				forward_mutator_t<iterator_d, element_d>::_pointer,
+			},
+			// forward_mutator_a
+			forward_mutator_t<iterator_d, element_d>::equal_iterator,
+			forward_mutator_t<iterator_d, element_d>::get,
+			forward_mutator_t<iterator_d, element_d>::set,
+			forward_mutator_t<iterator_d, element_d>::increment,
+			forward_mutator_t<iterator_d, element_d>::_operator_star,
+			forward_mutator_t<iterator_d, element_d>::_operator_arrow,
+		};
+		return &operations;
+	}
+
+	template <typename iterator_d, typename element_d>
+	forward_mutator_o<element_d> const* forward_mutator_t<iterator_d, element_d>::_pointer_operations()
+	{
+		static forward_mutator_o<element_d> operations = []()
+		{
+			forward_mutator_o<element_d> ops = *forward_mutator_t<iterator_d, element_d>::_operations();
+			ops._copy = thing_t::_no_copy;
+			return ops;
+		}();
+		return &operations;
+	}
+
+	// any_a
+	template <typename iterator_d, typename element_d>
+	bool forward_mutator_t<iterator_d, element_d>::is(con<> const& me,
+		con<> const& abstraction)
+	{
+		// abstraction.cat in me.cats
+		auto const abc = abstraction.o->cat;
+		return abc == any_a::cat ||
+			abc == forward_mutator_a<element_d>::cat;
+	}
+
+	template <typename iterator_d, typename element_d>
+	bool forward_mutator_t<iterator_d, element_d>::as(con<> const& me,
+		var<> const& abstraction)
+	{
+		if (!forward_mutator_t<iterator_d, element_d>::is(me, abstraction))
+		{
+			return false;
+		}
+		bool const as_pointer = abstraction.o->_pointer(abstraction);
+		abstraction = me;
+		if (abstraction.o->_pointer(abstraction) != as_pointer)
+		{
+			if (as_pointer)
+			{
+				abstraction.mut();
+			}
+			abstraction.o->_set_pointer(abstraction, as_pointer);
+			if (!as_pointer)
+			{
+				abstraction.mut();
+			}
+		}
+		return true;
+	}
+
+	template <typename iterator_d, typename element_d>
+	var<symbol_a> forward_mutator_t<iterator_d, element_d>::type(con<> const& me)
+	{
+		static auto r = sym("strange::forward_mutator");
+		return r;
+	}
+
+	template <typename iterator_d, typename element_d>
+	bool forward_mutator_t<iterator_d, element_d>::equal(con<> const& me,
+		con<> const& other)
+	{
+		auto const oi = other.dynamic<con<forward_mutator_a<element_d>>>();
+		if (oi.o->something(oi))
+		{
+			return forward_mutator_t<iterator_d, element_d>::equal_iterator(me.reference<con<forward_mutator_a<element_d>>>(), oi);
+		}
+		return thing_t::equal(me, other);
+	}
+
+	template <typename iterator_d, typename element_d>
+	void forward_mutator_t<iterator_d, element_d>::_copy(con<> const& me,
+		var<> const& copy)
+	{
+		new forward_mutator_t<iterator_d, element_d>{ copy, me };
+		forward_mutator_t<iterator_d, element_d>::_clone(me, copy);
+	}
+
+	template <typename iterator_d, typename element_d>
+	void forward_mutator_t<iterator_d, element_d>::_set_pointer(con<> const& me,
+		bool is_pointer)
+	{
+		me.o = is_pointer ? forward_mutator_t<iterator_d, element_d>::_pointer_operations() : forward_mutator_t<iterator_d, element_d>::_operations();
+	}
+
+	// forward_mutator_a
+	template <typename iterator_d, typename element_d>
+	element_d forward_mutator_t<iterator_d, element_d>::get(con<forward_mutator_a<element_d>> const& me)
+	{
+		return *(static_cast<forward_mutator_t<iterator_d, element_d>*>(me.t)->iterator_);
+	}
+
+	template <typename iterator_d, typename element_d>
+	void forward_mutator_t<iterator_d, element_d>::set(con<forward_mutator_a<element_d>> const& me,
+		element_d const& element)
+	{
+		*(static_cast<forward_mutator_t<iterator_d, element_d>*>(me.t)->iterator_) = element;
+	}
+
+	template <typename iterator_d, typename element_d>
+	void forward_mutator_t<iterator_d, element_d>::increment(var<forward_mutator_a<element_d>> const& me)
+	{
+		me.mut();
+		++(static_cast<forward_mutator_t<iterator_d, element_d>*>(me.t)->iterator_);
+	}
+
+	template <typename iterator_d, typename element_d>
+	element_d& forward_mutator_t<iterator_d, element_d>::_operator_star(con<forward_mutator_a<element_d>> const& me)
+	{
+		return (static_cast<forward_mutator_t<iterator_d, element_d>*>(me.t)->iterator_).operator*();
+	}
+
+	template <typename iterator_d, typename element_d>
+	element_d* forward_mutator_t<iterator_d, element_d>::_operator_arrow(con<forward_mutator_a<element_d>> const& me)
+	{
+		return (static_cast<forward_mutator_t<iterator_d, element_d>*>(me.t)->iterator_).operator->();
+	}
+
+	// instantiation
+	template struct forward_mutator_t<std::vector<int64_t>::iterator, int64_t>;
+}
---
+++ sauce/core/collections/iterators/things/strange__forward_mutator_t.h
@@ -0,0 +1,115 @@
+#ifndef COM_ONEISH__STRANGE__FORWARD_MUTATOR_T_H
+#define COM_ONEISH__STRANGE__FORWARD_MUTATOR_T_H
+
+namespace strange
+{
+	template <typename iterator_d, typename element_d>
+	struct forward_mutator_t : thing_t
+	{
+		iterator_d iterator_;
+
+	protected:
+		inline forward_mutator_t(any_a const& me)
+			: thing_t{ me }
+			, iterator_{}
+		{
+			me.o = forward_mutator_t<iterator_d, element_d>::_operations();
+		}
+
+		inline forward_mutator_t(any_a const& me,
+			iterator_d const& iterator)
+			: thing_t{ me }
+			, iterator_{ iterator }
+		{
+			me.o = forward_mutator_t<iterator_d, element_d>::_operations();
+		}
+
+		inline forward_mutator_t(any_a const& me,
+			any_a const& original)
+			: thing_t{ me, original }
+			, iterator_{ static_cast<forward_mutator_t<iterator_d, element_d>*>(original.t)->iterator_ }
+		{
+			me.o = forward_mutator_t<iterator_d, element_d>::_operations();
+		}
+
+		forward_mutator_t(forward_mutator_t const&) = delete;
+
+		forward_mutator_t& operator=(forward_mutator_t const&) = delete;
+
+	private:
+		// forward_mutator_o
+		static forward_mutator_o<element_d> const* _operations();
+		static forward_mutator_o<element_d> const* _pointer_operations();
+
+	public:
+		// inline
+		static inline bool equal_iterator(con<forward_mutator_a<element_d>> const& me,
+			con<forward_mutator_a<element_d>> const& other)
+		{
+			//TODO check other iterator type is compatible
+			return static_cast<forward_mutator_t<iterator_d, element_d>*>(me.t)->iterator_ ==
+				static_cast<forward_mutator_t<iterator_d, element_d>*>(other.t)->iterator_;
+		}
+
+	protected:
+		// any_a
+		static bool is(con<> const& me,
+			con<> const& abstraction);
+
+		static bool as(con<> const& me,
+			var<> const& abstraction);
+
+		static var<symbol_a> type(con<> const& me);
+
+		static bool equal(con<> const& me,
+			con<> const& other);
+
+		static void _copy(con<> const& me,
+			var<> const& copy);
+
+		static void _set_pointer(con<> const& me,
+			bool is_pointer);
+
+		// forward_mutator_a
+		static element_d get(con<forward_mutator_a<element_d>> const& me);
+
+		static void set(con<forward_mutator_a<element_d>> const& me,
+			element_d const& element);
+
+		static void increment(var<forward_mutator_a<element_d>> const& me);
+
+		static element_d& _operator_star(con<forward_mutator_a<element_d>> const& me);
+
+		static element_d* _operator_arrow(con<forward_mutator_a<element_d>> const& me);
+
+	public:
+		// creators
+		static inline rat<forward_mutator_a<element_d>> create_default()
+		{
+			any_a r;
+			new forward_mutator_t<iterator_d, element_d>{ r };
+			forward_mutator_t<iterator_d, element_d>::_initialise(reinterpret_cast<var<> const&>(r));
+			return rat<forward_mutator_a<element_d>>{ reinterpret_cast<forward_mutator_a<element_d>&>(r) };
+		}
+
+		static inline rat<forward_mutator_a<element_d>> create(iterator_d const& iterator)
+		{
+			any_a r;
+			new forward_mutator_t<iterator_d, element_d>{ r, iterator };
+			forward_mutator_t<iterator_d, element_d>::_initialise(reinterpret_cast<var<> const&>(r));
+			return rat<forward_mutator_a<element_d>>{ reinterpret_cast<forward_mutator_a<element_d>&>(r) };
+		}
+
+		static inline rat<forward_mutator_a<element_d>> create_from_range(con<range_a<>> const& range)
+		{
+			return create_default();
+		}
+
+		static inline rat<forward_mutator_a<element_d>> unpack(con<container_a> const& container)
+		{
+			return create_default();
+		}
+	};
+}
+
+#endif
